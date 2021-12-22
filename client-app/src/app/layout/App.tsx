d1ea04b58cfa79e02b1f8fbdb0be1fef946a627f
elavagni <>
2021-12-21 20:44:34 -0600

    Show list of activities and details

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,28 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/dashboard/AtivityDashboard';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then((response) => {
+      console.log(response);
+      setActivities(response.data);
+    });
+  }, []);
+
+  return (
+    <Fragment>
+      <NavBar />
+      <Container style={{ marginTop: '7em' }}>
+        <ActivityDashboard activities={activities} />
+      </Container>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ client-app/src/app/layout/NavBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button, Container, Menu } from 'semantic-ui-react';
+
+export default function NavBar() {
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <Menu.Item header>
+          <img src="/assets/logo.png" alt="logo" style={{ marginRight: '10px' }} />
+          Reactivities
+        </Menu.Item>
+        <Menu.Item name="Activities"></Menu.Item>
+        <Menu.Item>
+          <Button positive content="Create Activity" />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+}
