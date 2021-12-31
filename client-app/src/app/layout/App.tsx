f1e0d9ebea4317d06355cd9a4571a9af614c1e12
eric <elavagnim@gmail.com>
2021-12-30 22:44:32 -0600

    Use react-router to navigate between components

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,26 +1,36 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/AtivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Route, useLocation } from 'react-router-dom';
+import ActivityForm from '../../features/activities/form/ActivityForm';
+import HomePage from '../../features/home/HomePage';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 function App() {
-  const { activityStore } = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-  if (activityStore.loadingInitial) return <LoadingComponent content="Loading application" />;
+  const location = useLocation();
 
   return (
     <Fragment>
-      <NavBar />
-      <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard />
-      </Container>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path={'/(.+)'}
+        render={() => (
+          <>
+            <NavBar />
+            <Container style={{ marginTop: '7em' }}>
+              <Route exact path="/activities" component={ActivityDashboard} />
+              <Route path="/activities/:id" component={ActivityDetails} />
+              <Route
+                key={location.key}
+                path={['/createActivity', '/manage/:id']}
+                component={ActivityForm}
+              />
+            </Container>
+          </>
+        )}
+      />
     </Fragment>
   );
 }
---
+++ client-app/src/features/home/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react-dom';
+import { Link } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+export default function HomePage() {
+  return (
+    <Container style={{ marginTop: '7em' }}>
+      <h1>Home page</h1>
+      <h3>
+        Go to <Link to="/activities">Activities</Link>
+      </h3>
+    </Container>
+  );
+}
