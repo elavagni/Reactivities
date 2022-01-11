7fe0946a26844360c4e548f8fa454be7e408a26f
eric <elavagnim@gmail.com>
2022-01-10 20:07:43 -0600

    Setup error handling for the backend

--- API/Startup.cs
+++ API/Startup.cs
@@ -17,6 +17,8 @@ using MediatR;
 using Application.Activities;
 using Application.Core;
 using API.Extensions;
+using FluentValidation.AspNetCore;
+using API.Middleware;
 
 namespace API
 {
@@ -32,16 +34,20 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddFluentValidation(config => {
+              config.RegisterValidatorsFromAssemblyContaining<Create>();
+            });
             services.AddApplicationServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+      
+            app.UseMiddleware<ExceptionMiddleware>();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,33 +1,44 @@
+using Application.Core;
 using Domain;
+using FluentValidation;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
 {
     public class Create
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Activity Activity { get; set; }
         }
 
-        public class Handler : IRequestHandler<Command>
-        {
+    public class CommnadValidator : AbstractValidator<Command>
+    {
+      public CommnadValidator()
+      {
+        RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
+      }
+    }
+
+    public class Handler : IRequestHandler<Command, Result<Unit>>
+    {
             private readonly DataContext _context;
             public Handler(DataContext context)
             {
                 _context = context;
             }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 _context.Activities.Add(request.Activity);
 
-                await _context.SaveChangesAsync();
+                var result = await _context.SaveChangesAsync() > 0;
+
+                if (!result) return Result<Unit>.Failure("Failed to create activity");
 
                 //return this value just to indicate that the request is done
-                return Unit.Value;
+                return Result<Unit>.Success(Unit.Value);
             }
         }
 
