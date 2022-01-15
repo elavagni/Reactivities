720991b3e1adf18612049e8aa80f8c6b5adb3329
eric <elavagnim@gmail.com>
2022-01-15 14:46:43 -0600

    Use yup and formik for form validation

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,11 +1,12 @@
 import { observer } from 'mobx-react-lite';
 import React, { ChangeEvent, useEffect, useState } from 'react';
 import { Link, useHistory, useParams } from 'react-router-dom';
-import { Button, Segment } from 'semantic-ui-react';
+import { Button, FormField, Label, Segment } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
 import { v4 as uuid } from 'uuid';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 export default observer(function ActivityForm() {
   const history = useHistory();
@@ -28,33 +29,30 @@ export default observer(function ActivityForm() {
     if (id) loadActivity(id).then((activity) => setActivity(activity!));
   }, [id, loadActivity]);
 
-  // async function handleSubmit() {
-  //   if (activity.id.length === 0) {
-  //     let newActivity = {
-  //       ...activity,
-  //       id: uuid(),
-  //     };
-  //     await createActivity(newActivity);
-  //     history.push(`/activities/${newActivity.id}`);
-  //   } else {
-  //     await updateActivity(activity);
-  //     history.push(`/activities/${activity.id}`);
-  //   }
-  // }
-
-  // function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-  //   const { name, value } = event.target;
-  //   setActivity({ ...activity, [name]: value });
-  // }
+  const validationSchema = Yup.object({
+    title: Yup.string().required('The activity title is required'),
+  });
 
   if (loadingInitial) return <LoadingComponent content="Loading activity..." />;
 
   return (
-    <Formik enableReinitialize initialValues={activity} onSubmit={(values) => console.log(values)}>
-      {({ values: activity, handleChange, handleSubmit }) => (
+    <Formik
+      validationSchema={validationSchema}
+      enableReinitialize
+      initialValues={activity}
+      onSubmit={(values) => console.log(values)}
+    >
+      {({ handleSubmit }) => (
         <Segment clearing>
           <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
-            <Field placeholder="Title" name="title" />
+            <FormField>
+              <Field placeholder="Title" name="title" />
+              <ErrorMessage
+                name="title"
+                render={(error) => <Label basic color="red" content={error} />}
+              ></ErrorMessage>
+            </FormField>
+
             <Field placeholder="Description" name="description" />
             <Field placeholder="Category" name="category" />
             <Field type="date" placeholder="Date" name="date" />
