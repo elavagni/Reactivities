9de9147ef9c7bd61567630b173d4c6eecd276285
eric <elavagnim@gmail.com>
2022-01-17 18:17:40 -0600

    Setup AspNetCore Identity

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,6 +38,7 @@ namespace API
               config.RegisterValidatorsFromAssemblyContaining<Create>();
             });
             services.AddApplicationServices(_config);
+            services.AddIdentityServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
