aa7ca8328615fffa82c8d8074301150a9cf9e4dd
eric <>
2022-02-26 19:06:24 -0600

    Add comments to projects

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,30 +1,37 @@
 using Application.Activities;
+using Application.Comments;
 using AutoMapper;
 using Domain;
 
 namespace Application.Core
 {
-  public class MappingProfiles : Profile
-  {
-    public MappingProfiles()
+    public class MappingProfiles : Profile
     {
-      CreateMap<Activity, Activity>();
-      //d goes for destination, o stands for options, and s goes for source
-      CreateMap<Activity, ActivityDto>()
-        .ForMember(d => d.HostUsename, o => o
-          .MapFrom(s => s.Attendees
-            .FirstOrDefault(x => x.IsHost).AppUser.UserName));
+        public MappingProfiles()
+        {
+            CreateMap<Activity, Activity>();
+            //d goes for destination, o stands for options, and s goes for source
+            CreateMap<Activity, ActivityDto>()
+              .ForMember(d => d.HostUsename, o => o
+                .MapFrom(s => s.Attendees
+                  .FirstOrDefault(x => x.IsHost).AppUser.UserName));
 
-      CreateMap<ActivityAttendee, AttendeeDto>()
-          .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
-          .ForMember(d => d.UserName, o => o.MapFrom(s => s.AppUser.UserName))
-          .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio))
-          .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
+            CreateMap<ActivityAttendee, AttendeeDto>()
+                .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
+                .ForMember(d => d.UserName, o => o.MapFrom(s => s.AppUser.UserName))
+                .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio))
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
 
-      //Map the AppUser object into the Profile object.  Since AppUser does not have a image property, use the url of the first image
-      //im Photos
-      CreateMap<AppUser, Profiles.Profile>()
-          .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url));
+            //Map the AppUser object into the Profile object.  Since AppUser does not have a image property, use the url of the first image
+            //in Photos
+            CreateMap<AppUser, Profiles.Profile>()
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url));
+
+            CreateMap<Comment, CommentDto>()
+                .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.Author.DisplayName))
+                .ForMember(d => d.UserName, o => o.MapFrom(s => s.Author.UserName))
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.Author.Photos.FirstOrDefault(x => x.IsMain).Url));
+
+        }
     }
-  }
 }
