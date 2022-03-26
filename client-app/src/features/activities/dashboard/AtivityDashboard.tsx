d9cce1646cf879155da1af9a814ac05d8671c339
eric <>
2022-03-26 12:17:41 -0500

    improve ux by using activity placeholder

--- client-app/src/features/activities/dashboard/AtivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/AtivityDashboard.tsx
@@ -7,6 +7,7 @@ import { PagingParams } from '../../../app/models/pagination';
 import { useStore } from '../../../app/stores/store';
 import ActivityFilters from './ActivityFilters';
 import ActivityList from './ActivityList';
+import ActivityListItemPlaceholder from './ActivityListItemPlaceholder';
 
 export default observer(function ActivityDashboard() {
   const { activityStore } = useStore();
@@ -24,24 +25,26 @@ export default observer(function ActivityDashboard() {
     if (activityRegistry.size <= 1) loadActivities();
   }, [activityRegistry.size, loadActivities]);
 
-  //TODO: rename loadingInitial
-  //do not show the loading indicator if we are loading the next page of results
-  if (activityStore.loadingInitial && !loadingNextPage)
-    return <LoadingComponent content="Loading activities..." />;
-
   return (
     <Grid>
       <Grid.Column width="10">
-        <InfiniteScroll
-          pageStart={0}
-          loadMore={handleGetNextPage}
-          hasMore={
-            !loadingNextPage && !!pagination && pagination.currentPage < pagination.totalPages
-          }
-          initialLoad={false}
-        >
-          <ActivityList />
-        </InfiniteScroll>
+        {activityStore.loadingInitial && !loadingNextPage ? (
+          <>
+            <ActivityListItemPlaceholder />
+            <ActivityListItemPlaceholder />
+          </>
+        ) : (
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={handleGetNextPage}
+            hasMore={
+              !loadingNextPage && !!pagination && pagination.currentPage < pagination.totalPages
+            }
+            initialLoad={false}
+          >
+            <ActivityList />
+          </InfiniteScroll>
+        )}
       </Grid.Column>
       <Grid.Column width="6">
         <ActivityFilters />
