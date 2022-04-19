c4495e35b1357c38337d22c7d6dcce8b0d816cde
eric <elavagnim@gmail.com>
2022-04-18 20:21:09 -0500

    Update test to open dialogs to login and register

--- client-app/cypress/integration/login.spec.js
+++ client-app/cypress/integration/login.spec.js
@@ -12,9 +12,32 @@ describe('Login to the application', () => {
       .should('have.attr', 'src', '/assets/logo.png');
   });
 
-  it('displays login and register buttons', () => {
+  it('displays login and register dialogs', () => {
+    //Check for Login and Register buttons
     cy.get('button').should('have.length', 2);
     cy.contains('Login!');
     cy.contains('Register');
+    //Display Login form
+    cy.contains('Login!').click();
+    cy.get('div.ui.dimmer')
+      .contains('Login to Reactivities')
+      .get('input[name=email]')
+      .get('input[name=password]');
+    cy.contains('Login!');
+    //Close form
+    cy.get('body').click('topLeft');
+    cy.get('div.ui.dimmer').should('not.exist');
+    //Display Register form
+    cy.contains('Register').click();
+    cy.get('div.ui.dimmer')
+      .contains('Sign up to Reactivities!')
+      .get('input[name=displayName]')
+      .get('input[name=userName]')
+      .get('input[name=email]')
+      .get('input[name=password]');
+    cy.contains('Register');
+    //Close form
+    cy.get('body').click('topLeft');
+    cy.get('div.ui.dimmer').should('not.exist');
   });
 });
